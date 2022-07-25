c624344446bfb9cb7002c02ab675c2ec570f15dc
st4rbrain <abhidstarboy@gmail.com>
2022-07-25 16:35:39 +0530

    modified hardhat.config.js by adding networks and etherscan objects

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: "https://small-bold-dream.matic-testnet.discover.quiknode.pro/4e7c4314ce145b2aae49af69f438667ba35f897d/",
+      accounts: ['a525651d1afcc51f70873b085f106cc1d16383681de1620c782edb1abb0195bf']
+    },
+  },
+  etherscan: {
+    apiKey: 'Q33YBDK4T9ME5V13WVTAEHKKECU7URU4GF'
+  }
 };
