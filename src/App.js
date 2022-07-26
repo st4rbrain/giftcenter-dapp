13a13f8f15be6dff4495f46a08eae86b35c68145
st4rbrain <abhidstarboy@gmail.com>
2022-07-26 14:49:54 +0530

    modified app.js by adding some elements and app.css by adding some styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello there!</h1>
-      <h1>How u doin'?</h1>
+    <div className='outerbox'>
+      <div className="App">
+        <p className="heading">GiftCenter</p>
+        <div>
+          <p className='subhead'>A place to show some love to your friends and family through the blockchain technology</p>
+        </div>
+        <div className='datafield'>
+          <p className='label'>Recipient Address:</p>
+          <input type="text" name='address'></input>
+          <p className='label'>Message:</p>
+          <textarea name='msg' className='msginput' rows="6" cols="35"></textarea>
+          <p className='label'>Amount:</p>
+          <input type="text" name='amount'></input>
+          <button className='sendGift'>Send Gift</button>
+        </div>
+        <div className='imagediv'>
+        </div>
+      </div>
     </div>
   );
 }
