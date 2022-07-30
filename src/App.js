f596639ff26e3ca08328556aca9024dd8466d8cb
st4rbrain <abhidstarboy@gmail.com>
2022-07-30 15:57:11 +0530

    continued designing in the css file

--- src/App.js
+++ src/App.js
@@ -1,113 +1,10 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import { useMoralis, useWeb3ExecuteFunction } from 'react-moralis';
+// import { useMoralis, useWeb3ExecuteFunction } from 'react-moralis';
 
 function App() {
 
-  const { authenticate, isAuthenticated, isAuthenticating, logout, Moralis } = useMoralis();
-  const contractProcessor = useWeb3ExecuteFunction();
-  const [senderAddress, setSenderAddress] = useState();
-
-  useEffect(() => {
-    if(isAuthenticated){
-      document.getElementById('showadd').value = senderAddress;
-    }else {
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isAuthenticated]);
-
-
-  const login = async () => {
-    if (!isAuthenticated) {
-      await authenticate({signingMessage: "Log in using Moralis" })
-        .then((user) => {
-          console.log("logged in user:", user);
-          console.log(user.get("ethAddress"));
-          setSenderAddress(user.get('ethAddress'));
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  }
-
-  const logOut = async () => {
-    await logout();
-    console.log("logged out");
-  }
-
-  const gift = async () => {
-    let options = {
-      contractAddress: "0x7D2D054b8A80a21F0404B60d019868a5b257A028",
-      functionName: "sendGift",
-      abi: [
-        {
-          "inputs":[
-            {
-              "internalType":"address",
-              "name":"_recipient",
-              "type":"address"
-            },
-            {
-              "internalType":"string",
-              "name":"_message",
-              "type":"string"
-            }
-          ],
-          "name":"sendGift",
-          "outputs":[],
-          "stateMutability":"payable",
-          "type":"function"
-        }
-      ],
-      params: {
-        _recipient: document.getElementById('address').value,
-        _message: document.getElementById('msg').value,
-      },
-      msgValue: Moralis.Units.ETH(document.getElementById('amt').value)
-    }
-
-    await contractProcessor.fetch({
-      params: options,
-      onSuccess: () => {
-        console.log("Gift sent successfully!");
-      },
-      onError: (error) => {
-        alert(error.data.msg);
-      },
-    });
-    
-  }
-
   return (
-    // <div>
-    //   <input type="button" className="loginbtn" onClick={login} value="Connect Wallet" id="showadd"></input>
-    //   <input type='button' className="logoutbtn"onClick={logOut} disabled={isAuthenticating} value="Disconnect"></input>
-    //   <div className='outerbox'>
-    //     <div className="App">
-    //       <div className="heading">GiftCenter</div>
-    //       <div className='subhead'>A place to show some love to your friends and family through the blockchain technology</div>
-    //       <div className='datafield'>
-    //         <div className='label'>Recipient Address:</div> 
-    //         <input className='addinput' type="text" id='address'></input>
-    //         <div className='label'>Message:</div>
-    //         <textarea id='msg' className='msginput' rows="5" cols="62"></textarea>
-    //         <div className='label'>Amount:</div>
-    //         <div className='token'>
-    //           <input className='amtinput' type="text" id='amt'></input>
-    //           <input type="button" className='tokendrop' value='MATIC'></input>
-    //         </div>
-    //         <button className='giftbtn' onClick={() => {
-    //           if(isAuthenticated){
-    //             gift();
-    //           }else{
-    //             alert("Connect your Wallet");
-    //           }
-    //         }}>Send Gift</button>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
     <div>
       <nav class="navbar">
         <div class="container">
@@ -130,12 +27,214 @@ function App() {
               <div className="hero">
                 <h5>This is a </h5>
                 <h1>Gifting Service</h1>
-                <h5>that runs on the Blockchain</h5>
+                <h5>on top of the Blockchain</h5>
               </div>
               <div className='headimg'>
               </div>
           </div>
       </header>
+      <section className='giftlist'>
+        <div class='container'>
+          <div className='giftheading'>
+            <div className='gftheader'>
+              Gifted Items
+            </div>
+            <div className='gftdesc'>
+              This is a record of all the gifts that have been sent over the past decade
+            </div>
+          </div>
+          <div className='topgifts'>
+            <div className='title'><h1>Top Gifted Amounts</h1></div>
+            <div className='gifts'>
+              <div className='gift'>
+                <div className='giftline'>
+                  <div className='label'>From</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>To</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>Amount</div>
+                  <div className='giftdata'>0000000</div>
+                </div>
+              </div>
+              <div className='gift'>
+                <div className='giftline'>
+                  <div className='label'>From</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>To</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>Amount</div>
+                  <div className='giftdata'>0000000</div>
+                </div>
+              </div>
+              <div className='gift'>
+                <div className='giftline'>
+                  <div className='label'>From</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>To</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>Amount</div>
+                  <div className='giftdata'>0000000</div>
+                </div>
+              </div>
+              <div className='gift'>
+                <div className='giftline'>
+                  <div className='label'>From</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>To</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>Amount</div>
+                  <div className='giftdata'>0000000</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='dailygifts'>
+            <div className='title'><h1>Latest Gifts</h1></div>
+            <div className='gifts'>
+            <div className='gift'>
+                <div className='giftline'>
+                  <div className='label'>From</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>To</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>Amount</div>
+                  <div className='giftdata'>0000000</div>
+                </div>
+              </div>
+              <div className='gift'>
+                <div className='giftline'>
+                  <div className='label'>From</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>To</div>
+                  <div className='giftdata'>Myself</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>Amount</div>
+                  <div className='giftdata'>0000000</div>
+                </div>
+              </div>
+              <div className='gift'>
+                <div className='giftline'>
+                  <div className='label'>From</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>To</div>
+                  <div className='giftdata'>Myself</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>Amount</div>
+                  <div className='giftdata'>0000000</div>
+                </div>
+              </div>
+              <div className='gift'>
+                <div className='giftline'>
+                  <div className='label'>From</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>To</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>Amount</div>
+                  <div className='giftdata'>0000000</div>
+                </div>
+              </div>
+              <div className='gift'>
+                <div className='giftline'>
+                  <div className='label'>From</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>To</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>Amount</div>
+                  <div className='giftdata'>0000000</div>
+                </div>
+              </div>
+              <div className='gift'>
+                <div className='giftline'>
+                  <div className='label'>From</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>To</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>Amount</div>
+                  <div className='giftdata'>0000000</div>
+                </div>
+              </div>
+              <div className='gift'>
+                <div className='giftline'>
+                  <div className='label'>From</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>To</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>Amount</div>
+                  <div className='giftdata'>0000000</div>
+                </div>
+              </div>
+              <div className='gift'>
+                <div className='giftline'>
+                  <div className='label'>From</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>To</div>
+                  <div className='giftdata'>0x5a8e9D4B757646097366f978999c79ef87228A99</div>
+                </div>
+                <div className='giftline'>
+                  <div className='label'>Amount</div>
+                  <div className='giftdata'>0000000</div>
+                </div>
+              </div>
+            </div>
+            <div className='pagination'>
+              <h1>Switch Page</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer className='footer'>
+        <div className='container'>
+          <div className='exp'>
+          <div className='title'>
+            <h1>Did you send someone a gift?</h1>
+          </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { MoralisProvider } from 'react-moralis';
+// import { MoralisProvider } from 'react-moralis';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MoralisProvider serverUrl="https://mxotjtbno0je.usemoralis.com:2053/server" appId="014Lecm27TKCWGoY6pfB5ky5vckyAWehsAdUWDh7">
+    {/* <MoralisProvider serverUrl="https://mxotjtbno0je.usemoralis.com:2053/server" appId="014Lecm27TKCWGoY6pfB5ky5vckyAWehsAdUWDh7"> */}
       <App />
-    </MoralisProvider>
+    {/* </MoralisProvider> */}
   </React.StrictMode>
 );
 
