0d067add1959cfdd4325a1b09488da83d8b028af
st4rbrain <abhidstarboy@gmail.com>
2022-08-15 12:15:45 +0530

    made heroku related changes

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import fs from 'fs';
+import dotenv from 'dotenv';
 import giftsRoutes from './routes/gifts.js';
 
 const app = express();
+dotenv.config()
 
 app.use(cors());
 app.use(express.json());
 app.use('/gifts', giftsRoutes);
 
+app.get('/', (req, res) => {
+    res.send("Gift Center API");
+});
 
-const CONNECTION_URL = "mongodb+srv://starbrain:mongodbpass@cluster0.g5hwgnb.mongodb.net/GiftCenterdb?retryWrites=true&w=majority";
+
 const PORT = process.env.PORT || 3001;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log("Server running on port", PORT)))
     .catch((e) => console.log(console.log(e.message)));
 
